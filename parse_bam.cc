85a366b0d0f3150a2bc28b03a63ca0de20eb71d4
Avinash Ramu <trackavinash@gmail.com>
2015-11-09 16:27:55 -0600

    Initial commit.

---
+++ parse_bam.cc
@@ -0,0 +1,81 @@
+/*  parse_bam.cc -- Example BAM parser
+
+    Copyright (c) 2015, The Griffith Lab
+
+    Author: Avinash Ramu <aramu@genome.wustl.edu>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.  */
+
+#include <iostream>
+
+#include "hts.h"
+#include "sam.h"
+
+using namespace std;
+
+int usage() {
+    cerr << "./parse_bam example.bam";
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        return usage();
+    }
+    string bam = string(argv[1]);
+    string region_ = "";
+    if(argc > 2) {
+        region_ = string(argv[2]);
+    }
+    if(!bam.empty()) {
+        //open BAM for reading
+        samFile *in = sam_open(bam.c_str(), "r");
+        if(in == NULL) {
+            throw runtime_error("Unable to open BAM/SAM file.");
+        }
+        //Load the index
+        hts_idx_t *idx = sam_index_load(in, bam.c_str());
+        if(idx == NULL) {
+            throw runtime_error("Unable to open BAM/SAM index."
+                                " Make sure alignments are indexed");
+        }
+        //Get the header
+        bam_hdr_t *header = sam_hdr_read(in);
+        //Initialize iterator
+        hts_itr_t *iter = NULL;
+        //Move the iterator to the region we are interested in
+        iter  = sam_itr_querys(idx, header, region_.c_str());
+        if(header == NULL || iter == NULL) {
+            sam_close(in);
+            throw runtime_error("Unable to iterate to region within BAM.");
+        }
+        //Initiate the alignment record
+        bam1_t *aln = bam_init1();
+        while(sam_itr_next(in, iter, aln) >= 0) {
+            cout << endl << aln->core.tid;
+            cout << endl << aln->core.pos;
+        }
+        hts_itr_destroy(iter);
+        hts_idx_destroy(idx);
+        bam_destroy1(aln);
+        bam_hdr_destroy(header);
+        sam_close(in);
+    }
+    return 0;
+}
