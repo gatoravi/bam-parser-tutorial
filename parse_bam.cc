d1e17bb09655734b18c4c1ca64c6baf9300b443e
Avinash Ramu <trackavinash@gmail.com>
2016-03-04 09:32:39 -0600

    Add header

--- parse_bam.cc
+++ parse_bam.cc
@@ -23,6 +23,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.  */
 
 #include <iostream>
+#include <stdexcept>
 
 #include "hts.h"
 #include "sam.h"
